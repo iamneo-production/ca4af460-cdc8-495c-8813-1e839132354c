dec98e8e31fc36ce85b6c54d5a0e1b980499d040
Debrup Mondal <57451228+debrupofficial365@users.noreply.github.com>
2021-10-21 16:54:01 +0530

    Update SpringappApplication.java

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -3,6 +3,7 @@ package com.examly.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//updated 
 @SpringBootApplication
 public class SpringappApplication {
 
